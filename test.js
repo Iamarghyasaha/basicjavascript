22e37ce876c85d1e05111f0fc842df2d0694c14c
Arghya <sahaarghya5678@gmail.com>
2023-12-03 21:13:20 +0530

    it is for test

---
+++ test.js
@@ -0,0 +1,23 @@
+class Engine{
+    constructor(parameter){
+        this.parameter = parameter;
+    }
+}
+class Tires{
+    constructor(parameter){
+        this.parameter = parameter;
+    }
+}
+class Car{
+    engine;
+    tires;
+    constructor(engine,tires){
+        this.engine = engine;
+        this.tires = tires;
+    }
+}
+var myEngine = new Engine('para')
+var myTires = new Tires('para');
+var myCar = new Car(myEngine,myTires);
+console.log(myCar.tires);
+
